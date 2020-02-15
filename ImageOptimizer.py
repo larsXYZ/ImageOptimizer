7c9cc663394578a59b144c05f6258fe8d1f5a606
larsXYZ <larsm.angelsen@gmail.com>
2020-02-15 01:41:28 +0100

    Finished for today

--- ImageOptimizer.py
+++ ImageOptimizer.py
@@ -1,21 +1,22 @@
+from scipy.ndimage.filters import gaussian_filter
 import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 
-target_image = imageio.imread(r"C:\Users\thehu\Documents\ImageOptimizer\Untitled.png")
+target_image = imageio.imread(r"C:\Users\thehu\Documents\ImageOptimizer\images\current_target.png")
 current_image = 255*np.ones(shape=target_image.shape, dtype=np.uint8)
 
 def measure_error(target_image, image):
     return np.sum(np.abs(target_image-image).flatten())
 
-def add_random_shape(image):
+def perform_random_operation(image):
 
     new_image = image.copy()
 
-    shape = np.random.choice(['Square', 'Circle'])
+    operation = np.random.choice(['Square', 'Circle', 'GaussBlur'])
     [im_x_size, im_y_size, _] = image.shape
 
-    if shape == 'Square':
+    if operation == 'Square':
         x0 = np.random.randint(0, im_x_size)
         y0 = np.random.randint(0, im_y_size)
 
@@ -28,7 +29,7 @@ def add_random_shape(image):
         color = np.random.randint(0, 255, size=3)
 
         new_image[x_min:x_max, y_min:y_max, :] = color[np.newaxis, np.newaxis, :]
-    elif shape == 'Circle':
+    elif operation == 'Circle':
         center_x = np.random.randint(1, im_x_size-1)
         center_y = np.random.randint(1, im_y_size-1)
 
@@ -40,30 +41,26 @@ def add_random_shape(image):
             for y in range(center_y-radius, center_y+radius+1):
                 if (center_x - x) ** 2 + (center_y - y) ** 2 < radius ** 2:
                     new_image[x, y] = color[np.newaxis, np.newaxis, :]
+    elif operation == 'GaussBlur':
+        sigma = np.random.randint(0, 10)
+        new_image = gaussian_filter(new_image, sigma=sigma)
 
-    new_error = measure_error(target_image, new_image)
-
-    return new_image, new_error
+    return new_image, operation
 
 N = 100000000
 for i in range(N):
-    old_error = measure_error(target_image, current_image)
 
-    best_error = old_error
-    candidate_image = current_image
+    current_error = measure_error(target_image, current_image)
+    new_image, operation = perform_random_operation(current_image)
+    new_error = measure_error(target_image, new_image)
 
-    for x in range(5):
+    if new_error < current_error:
+        current_image = new_image
 
-        new_image, error = add_random_shape(current_image)
+    print("{:<6}, {:<10} : {:<12} {}/{}".format(str(new_error < current_error), new_error, operation[:19], i, N))
 
-        if error < best_error:
-            best_error = error
-            candidate_image = new_image
-
-    current_image = candidate_image
-
-    print("{}/{}".format(i, N))
+    if i % 10 == 0:
+        plt.imsave("generated_images\web_image.png", arr=current_image)
 
     if i % 100 == 0:
-        plt.imshow(current_image)
-        plt.imsave("IM_{}.png".format(i), arr=current_image)
+        plt.imsave("generated_images\IM_{}.png".format(i), arr=current_image)
